90d41dfae8e26719716060051aaf4f6d1235ae70
kevin hu <hu.kevin.w@gmail.com>
2022-09-15 22:27:03 -0400

    Time: 120 ms (5.24%), Space: 42.4 MB (14.80%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -7,15 +7,13 @@ var generate = function(numRows) {
     if (numRows >= 1 ) res.push([1])
     if (numRows >= 2 ) res.push([1,1])
     for (let i = 2; i < numRows; i++) {
-        let insertIdx = i-1;
-        let prevRow = res[insertIdx]
+        let prevRow = res[i-1]
         
         let l = 0, r = i;
         let hold = [] 
         while ( l <= r ) {
-            let cal = l === 0 ? 1 : prevRow[l-1] + prevRow[l] 
-            hold[l] = cal
-            hold[r] = cal
+            hold[l] = l === 0 ? 1 : prevRow[l-1] + prevRow[l] 
+            hold[r] = l === 0 ? 1 : prevRow[l-1] + prevRow[l] 
             l++
             r--
         }
